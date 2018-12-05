864ec31725aaf92d07c07edf446193839906bdc6
Steven <stevenhuynh17@gmail.com>
2018-12-05 13:26:36 -0800

    starting over hw03

--- homework/src/hw03/EmployeeDataStore.java
+++ homework/src/hw03/EmployeeDataStore.java
@@ -1,13 +1,11 @@
 package hw03;
 
 import java.io.File;
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.ArrayList;
 import java.util.Collections;  // Collections.sort
+import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
 
@@ -21,19 +19,13 @@ import java.util.NoSuchElementException;
  */
 public class EmployeeDataStore {
     private String filename;
+    private List<Employee> employees = new LinkedList<Employee>();
     // TODO: Add any other members you might need
-    private ArrayList<Employee> employees = new ArrayList<Employee>();
-    private ArrayList<Employee> query = new ArrayList<Employee>();
 
     public EmployeeDataStore(String filename) throws IOException {
         // TODO
-    	try {
-    		this.filename = filename;
-            reloadDataStore();
-    	} catch(IOException err) {
-    		throw new IOException("Something went wrong with loading the file. Please check path of file." + err);
-    	}
-        
+        this.filename = filename;
+        saveDataStore();
     }
 
     /**
@@ -45,71 +37,38 @@ public class EmployeeDataStore {
      */
     public void reloadDataStore() throws IOException {
         // TODO
-    	Scanner scan = new Scanner(new File(this.filename));    	
-    	while(scan.hasNextLine()) {
-    		String name;
-    		String title;
-    		int basePay;
-    		int totalPay;
-        	
-    		name = scan.nextLine();
-    		title = scan.nextLine();
-    		basePay = Integer.parseInt(scan.nextLine());
-    		totalPay = Integer.parseInt(scan.nextLine());
-
-    		employees.add(new Employee(name, title, basePay, totalPay));
-    	}
-    	scan.close();
-    	Collections.sort(employees);
-//    	System.out.println(employees);
     }
 
     /**
      * Write the Employee objects in text format, sorted by name. This is kind of the inverse of .reloadDataStore().
-     * @throws FileNotFoundException
+     * @throws IOException
      */
-    public void saveDataStore() throws FileNotFoundException {
+    public void saveDataStore() throws IOException {
         // TODO: Save Employees in sorted order to this.filename;
-    	try {
-    		Scanner scan = new Scanner(new File(this.filename));    	
-        	while(scan.hasNextLine()) {
-        		String name;
-        		String title;
-        		int basePay;
-        		int totalPay;
-            	
-        		name = scan.nextLine();
-        		title = scan.nextLine();
-        		basePay = Integer.parseInt(scan.nextLine());
-        		totalPay = Integer.parseInt(scan.nextLine());
-
-        		employees.add(new Employee(name, title, basePay, totalPay));
-        	}
-        	scan.close();
-        	Collections.sort(employees);
-//        	BufferedWriter writer = new BufferedWriter(new FileWriter())
-    	} catch(FileNotFoundException err) {
-    		System.out.println("Check to see if the file used exists in the path provided:");
-    		System.out.println(err);
+    	File file = new File(this.filename);
+    	Scanner input = new Scanner(file);
+    	
+    	while(input.hasNextLine()) {
+    		String name = input.nextLine();
+    		String title = input.nextLine();
+    		int basePay = Integer.parseInt(input.nextLine());
+    		int totalPay = Integer.parseInt(input.nextLine());
+    		
+    		employees.add(new Employee(name, title, basePay, totalPay));
+    		
     	}
     	
-    	
-    }	
+    	Collections.sort(employees);
+    	input.close();
+    }
 
-	/**
+    /**
      * Returns a reference to an Employee with a particular name. If there's no such employee, return null;
      * @param name Name of employee, assuming all Employee.name's are unique.
      * @return A reference to an Employee in the DataStore. If it's modified, saving the DataStore will save changes.
      */
     public Employee getEmployee(String name) {
         // TODO
-    	for(int i=0; i < employees.size(); i++) {
-    		String current_value = employees.get(i).name;
-    		
-    		if(current_value.equals(name)) {
-    			return employees.get(i);
-    		}
-    	}
         return null;
     }
 
@@ -125,44 +84,6 @@ public class EmployeeDataStore {
         // TODO
         // TODO: Make sure when you throw an IllegalQueryException that you include helpful debug information.
         //   Think about what a human seeing your error message would like to know when their program crashes.
-    	try {	
-    		int start = 0;
-    		int finish = 0;
-    		
-    		for(int i=0; i < employees.size(); i++) {
-        		String current_value = employees.get(i).name;
-        		
-        		boolean A = current_value.equalsIgnoreCase(greaterThanName);
-        		boolean B = current_value.equalsIgnoreCase(lessThanName);
-
-        		if(A) {
-        			start = i + 1;
-        		} else if(B) {
-        			finish = i - 1;
-        		}	
-        	}
-    		
-    		if(start > finish) {
-    			throw new IllegalQueryException("The ranges you put in would not generate any possible profiles. Check to see if the first input is alphabetically first than the second input: ");
-//    		} else if(greaterThanName == "Apple" && lessThanName == "") {
-//    			throw new IllegalQueryException("Surely this works now");
-    		} 
-    		
-    		else {
-    			for(int i=start; i<=finish; i++) {
-    				query.add(employees.get(i));
-    			}
-    			System.out.println(start);
-    			System.out.println(finish);
-    			return query;
-    		}
-    	} catch(IllegalQueryException err) {
-    		System.out.println("SOMETHING IS WWRONG");
-    	}
-//    		return employees.subList(start, finish);
-		return query;
-
-    	
-//        return null;
+        return null;
     }
-    }
+}
--- homework/src/hw03/GradeOvenTest.java
+++ homework/src/hw03/GradeOvenTest.java
@@ -6,7 +6,10 @@ public class GradeOvenTest {
 	public static void main(String[] args) throws IOException {
 		String filename = "/Users/StevenH/Desktop/Coding/CS1B/homework/src/hw03/2014-foothill-cs-simplified.txt";
 		EmployeeDataStore store = new EmployeeDataStore(filename);
+		
 		Employee test = store.getEmployee("Bita Mazloom");
-		store.query("Apple", "");
+		
+//		store.query("Apple", "");
+//		System.out.println("TESTING");
 	}
 }
