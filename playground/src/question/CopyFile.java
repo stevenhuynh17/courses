f5ffda6227d70627c1fa326abf803b3195aef0e8
Steven <stevenhuynh17@gmail.com>
2018-12-12 10:02:25 -0800

    course completed

---
+++ playground/src/question/CopyFile.java
@@ -0,0 +1,18 @@
+package question;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+class CopyFile {
+    public static void main(String[] args) {
+    	String x = "STEVEN";
+    	for (int i = 0; i < x.length(); i++) {
+
+    		System.out.println(x.charAt(i));
+
+    	}
+    }
+}
